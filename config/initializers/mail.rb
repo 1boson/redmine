02a936ae4786e3dca369a16aca5be382372b6fa5
Iurii Marchenko <dorialan@gmail.com>
2012-07-21 01:41:46 +0300

    mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "127.0.0.1",
   :port                 => 25,
-  :domain               => "1boson.com",
+  :domain               => "rm.1boson.com",
   :enable_starttls_auto => false
 }
